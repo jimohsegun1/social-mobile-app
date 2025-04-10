0a54cb011bf5137d25fbda07c66d23624a723baf
jimohsegun1 <jimohsegunj@gmail.com>
2025-04-10 14:42:40 +0100

    integrating convex for database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,11 @@
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { useCallback, useEffect } from "react";
-import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 
 import * as NavigationBar from "expo-navigation-bar";
 import { Platform } from "react-native";
-import { tokenCache } from "@/cache";
 import InitialLayout from "@/components/InitialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-if (!publishableKey) {
-  throw new Error(
-    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
-  );
-}
 export default function RootLayout() {
   // update the native navigation bar on Android.
   useEffect(() => {
@@ -25,14 +16,12 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        <SafeAreaProvider>
-          <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-            <InitialLayout />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <ClerkAndConvexProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+          <InitialLayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkAndConvexProvider>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+
+
+export default {
+    providers: [
+      {
+        domain: "https://eternal-rooster-53.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
