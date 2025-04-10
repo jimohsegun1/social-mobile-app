991ebda526d76933131af7b52fcf09b235a77e78
jimohsegun1 <jimohsegunj@gmail.com>
2025-04-10 11:04:45 +0100

    Navigation tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useCallback, useEffect } from "react";
+
+import * as NavigationBar from "expo-navigation-bar";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  // update the native navigation bar on Android.
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync("#000000");
+      NavigationBar.setButtonStyleAsync("light");
+    }
+  }, []);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }}></Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
